7add2d1849a6a5c347a0830cbf76027e079994f6
CelepiYakup <135622873+CelepiYakup@users.noreply.github.com>
2023-12-16 14:59:33 +0300

    Updated main window centerilazed

--- main.py
+++ main.py
@@ -4,21 +4,37 @@ class Main(object):
     def __init__(self, master):
         self.master = master
         master.title("Library Management System")
-        master.geometry("1440x850+250+200")
 
-        #Main frames can be proceeded
+        screen_width = master.winfo_screenwidth()
+        screen_height = master.winfo_screenheight()
+
+        # Calculate X and Y offsets for center position
+        x_offset = (screen_width - 1440) // 2
+        y_offset = (screen_height - 850) // 2
+        master.geometry(f"1440x850+{x_offset}+{y_offset}")
+
+        # Main frame
         mainFrame = Frame(self.master)
         mainFrame.pack()
 
-        # Top frame
-        topFrame = Frame(mainFrame, width=1440, height=90, bg='orange2', relief=SUNKEN, padx=20, borderwidth=2)
-        topFrame.pack(side=BOTTOM, fill=X)
+        # Bottom frame
+        bottomFrame = Frame(mainFrame, width=1440, height=150, bg='orange2', relief=SUNKEN, padx=20, borderwidth=2)
+        bottomFrame.pack(side=BOTTOM, fill=X)
 
-        #center frame
-        centerFrame =Frame(mainFrame, width=1440,bg='#000000', relief=RIDGE, height=700)
+        # Top frame
+        topFrame = Frame(mainFrame, width=1440, height=150, bg='red', relief=RAISED, padx=20, borderwidth=2)
+        topFrame.pack(side=TOP, fill=X)
+
+        # Center frame
+        centerFrame = Frame(mainFrame, width=1440, bg='#000000', relief=RIDGE, height=700)
         centerFrame.pack(side=TOP)
 
+        # Creating center left and right frames
+        centerLeft = Frame(centerFrame, width=1008, height=700, bg='#212121', relief=SUNKEN, borderwidth=2)
+        centerLeft.pack(side=LEFT)
 
+        centerRight = Frame(centerFrame, width=432, height=700, bg='#313131', relief=SUNKEN, borderwidth=2)
+        centerRight.pack(side=RIGHT)
 
 if __name__ == "__main__":
     root = Tk()
